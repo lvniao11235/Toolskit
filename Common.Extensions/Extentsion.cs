a2647dfeb33810746bc3654601e39b20876aeec2
Joe Jiao (Beyondsoft Corporation) <v-yojiao@microsoft.com>
2018-09-12 09:49:07 +0800

    add files

---
+++ Common.Extensions/Extentsion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Common.Extensions
+{
+    public static class Extentsion
+    {
+        public static String ToJson(this Object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
---
+++ Toolskit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Extensions;
+
+namespace Toolskit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new { name = "lvniao", age = 12 };
+            Console.WriteLine(obj.ToJson());
+        }
+    }
+}
