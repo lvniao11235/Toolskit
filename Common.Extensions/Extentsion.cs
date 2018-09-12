678a46ae7799a1c26bcace38300b41b80d472389
Joe Jiao (Beyondsoft Corporation) <v-yojiao@microsoft.com>
2018-09-12 11:13:57 +0800

    add method

--- Common.Extensions/Extentsion.cs
+++ Common.Extensions/Extentsion.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Globalization;
 
 namespace Common.Extensions
 {
@@ -12,6 +13,49 @@ namespace Common.Extensions
         public static String ToJson(this Object obj)
         {
             return  JsonConvert.SerializeObject(obj);
+        }
+
+        public static String ToJson(this Object obj, params JsonConverter[] converters)
+        {
+            return JsonConvert.SerializeObject(obj, converters);
+        }
+
+        public static T Parse<T>(this Object obj, string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public static T Parse<T>(this Object obj, string json, params JsonConverter[] converters)
+        {
+            return JsonConvert.DeserializeObject<T>(json, converters);
+        }
+    }
+
+    public class DateTimeJsonConverter : JsonConverter
+    {
+        private string datetimeformat;
+
+        public DateTimeJsonConverter(string datetimeformat)
+        {
+            this.datetimeformat = datetimeformat;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.Equals(typeof(DateTime));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, 
+            object existingValue, JsonSerializer serializer)
+        {
+            String value = (String)reader.Value;
+            return DateTime.ParseExact(value, datetimeformat, CultureInfo.InvariantCulture);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, 
+            JsonSerializer serializer)
+        {
+            writer.WriteValue(((DateTime)value).ToString(datetimeformat));
         }
     }
 }
--- Toolskit/Program.cs
+++ Toolskit/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Common.Extensions;
+using Newtonsoft.Json;
 
 namespace Toolskit
 {
@@ -11,8 +12,20 @@ namespace Toolskit
     {
         static void Main(string[] args)
         {
-            var obj  = new { name = "lvniao", age = 12 };
-            Console.WriteLine(obj.ToJson());
+            var obj  = new Student(){ name = "lvniao", age = 12, birthday=DateTime.Now };
+            string str = obj.ToJson(new DateTimeJsonConverter("yyyyMMddHHmmss"));
+            Console.WriteLine(str);
+            var newobj = obj.Parse<Student>(str, new DateTimeJsonConverter("yyyyMMddHHmmss"));
+            Console.WriteLine(newobj.ToJson());
         }
+    }
+
+    public class Student
+    {
+        public string name { get; set; }
+
+        public int age { get; set; }
+
+        public DateTime birthday { get; set; }
     }
 }
